4b789e5994da3588a5f2a3d0d5019e22e535c9fb
Telon Yan <tjy2@twostep.cam.nist.gov>
2018-06-26 15:36:23 -0400

    Updated crystal-vis.py

--- crystal-vis.py
+++ crystal-vis.py
@@ -7,7 +7,10 @@ Color codes individual atoms by element
 import os
 
 #make a dictionary of all the elements and their respective "-r __ -g __ -b __ -a __" strings. -a may not be necessary
-elementColors = {"H" : "-r 1 -g 1 -b 1"}
+elementColors = {"H" : "-r 1 -g 1 -b 1",
+		 "O" : "-r 0.99 -g 0.001 -b 0.001",
+		 "PR": "-r 0.8 -g 0.8 -b 0.1",
+		 "NI": "-r 0.5 -g 0.5 -b 0.5"}
 rgbStrings = []
 xyzCoords = []
 
@@ -17,15 +20,18 @@ line = file.readline()
 while line != "":
 	splitLine = line.split()
 	print(splitLine)
-	xyzCoords.append(str(splitLine[3] + " " + splitLine[4] + " " + splitLine[5]))
-	currentRGBString = elementColors[splitLine[1]]
-	rgbStrings.append(currentRGBString)
+	xyzCoords.append(splitLine[3] + " " + splitLine[4] + " " + splitLine[5])
+	rgbStrings.append(elementColors[splitLine[2]])
 	line = file.readline()
 
 os.system("source /usr/local/HEV/.bashhev")
 os.system("hev")
-os.system("cat > master.sh")
+#os.system("cat > master.sh")
+os.system("rm molecule.savg")
+os.system("touch molecule.savg")
 
 #Create a string that can be run in the shell command
 for i in range(len(rgbStrings)):
-	os.system("savg-sphere | savg-scale 0.02 | savg-translate " + xyzCoords[i] + " | savg-color " + rgbStrings[i])
+	os.system("savg-sphere | savg-scale 0.02 | savg-translate " + xyzCoords[i] + " | savg-color " + rgbStrings[i] + ">> molecule.savg")
+
+os.system("irisfly --ex molecule.savg")
