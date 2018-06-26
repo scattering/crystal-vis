b9a2bacad77f59c3059fc6eba99dab9be7461c0c
Ryan <cho.ryan212@gmail.com>
2018-06-26 11:57:14 -0400

    Added to crystal-vis.py

--- crystal-vis.py
+++ crystal-vis.py
@@ -4,4 +4,17 @@ Authors: Ryan Cho and Telon Yan
 Render a 3D visualization of any given crystal given a .ctl file
 Color codes individual atoms by element
 """
+import os
 
+#make a dictionary of all the elements and their respective "-r __ -g __ -b __ -a __" strings
+
+#Read every x,y,z coordinate for each row
+#Create a string "savg-translate x y z" for the shell command
+
+command_features = "cat file.txt | savg-sphere
+
+#Read indices 5,6 for every row in file.txt
+
+os.system("source /usr/local/HEV/.bashhev")
+os.system("hev")
+os.system("
