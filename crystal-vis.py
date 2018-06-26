c5dbf5ca8e5f7581e2a64be5eae277a27256b84c
Telon Yan <tjy2@twostep.cam.nist.gov>
2018-06-26 15:15:59 -0400

    Updated crystal-vis.py

--- crystal-vis.py
+++ crystal-vis.py
@@ -6,15 +6,26 @@ Color codes individual atoms by element
 """
 import os
 
-#make a dictionary of all the elements and their respective "-r __ -g __ -b __ -a __" strings
+#make a dictionary of all the elements and their respective "-r __ -g __ -b __ -a __" strings. -a may not be necessary
+elementColors = {"H" : "-r 1 -g 1 -b 1"}
+rgbStrings = []
+xyzCoords = []
 
 #Read every x,y,z coordinate for each row
-#Create a string "savg-translate x y z" for the shell command
-
-command_features = "cat file.txt | savg-sphere | savg-sca" + 
-
-#Read indices 5,6 for every row in file.txt
+file = open("file.txt", "r")
+line = file.readline()
+while line != "":
+	splitLine = line.split()
+	print(splitLine)
+	xyzCoords.append(str(splitLine[3] + " " + splitLine[4] + " " + splitLine[5]))
+	currentRGBString = elementColors[splitLine[1]]
+	rgbStrings.append(currentRGBString)
+	line = file.readline()
 
 os.system("source /usr/local/HEV/.bashhev")
 os.system("hev")
-os.system("
+os.system("cat > master.sh")
+
+#Create a string that can be run in the shell command
+for i in range(len(rgbStrings)):
+	os.system("savg-sphere | savg-scale 0.02 | savg-translate " + xyzCoords[i] + " | savg-color " + rgbStrings[i])
