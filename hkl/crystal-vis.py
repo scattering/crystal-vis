edb89244e360f2869f2adece64db506a36a780dd
Telon Yan <tjy2@twostep.cam.nist.gov>
2018-06-27 09:24:26 -0400

    Deleted files

--- hkl/crystal-vis.py
+++ hkl/crystal-vis.py
@@ -1,8 +1,8 @@
 """
 Program name: crystal-vis.py
 Authors: Ryan Cho and Telon Yan
-Render a 3D visualization of any given crystal given a .ctl file
-Color codes individual atoms by element
+Render a 3D visualization of any hkl path taken given a .int file
+Draws lines between subsequent hkls
 """
 import os
 
