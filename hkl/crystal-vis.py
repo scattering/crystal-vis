66a3fc1417e32b3c7fde6f60b166887c7d257e64
Telon Yan <tjy2@twostep.cam.nist.gov>
2018-06-27 12:21:30 -0400

    Updated crystal-vis.py

--- hkl/crystal-vis.py
+++ hkl/crystal-vis.py
@@ -6,14 +6,21 @@ Draws lines between subsequent hkls
 """
 import os
 
+#List of Strings, e.g. ["1 1 1", "1 3 1", "1 3 3"]
 hklCoords = []
+#List of float lists (h, k, l)
+hklRealCoords = []
 
 #Read every x,y,z coordinate for each row
 file = open("hkl.txt", "r")
+file2 = open("lines.txt", "w")
+
 line = file.readline()
+
 while line != "":
 	splitLine = line.split()
 	print(splitLine)
+	hklRealCoords.append([float(int(splitLine[0])), float(int(splitLine[1])), float(int(splitLine[2]))])
 	hklCoords.append(str(float(int(splitLine[0]))) + " " + str(float(int(splitLine[1]))) + " " + str(float(int(splitLine[2]))))
 	line = file.readline()
 
@@ -22,9 +29,21 @@ os.system("hev")
 #os.system("cat > master.sh")
 os.system("rm molecule.savg")
 os.system("touch molecule.savg")
+os.system("rm lines.savg")
+os.system("touch lines.savg")
+
+#write "lines" into this file
+#os.system("lines > lines.savg")
+file2.write("lines\n")
 
 #Create a string that can be run in the shell command
 for i in range(len(hklCoords)):
-	os.system("savg-sphere | savg-scale 0.02 | savg-translate " + hklCoords[i] + " >> molecule.savg")
+	os.system("savg-sphere | savg-scale 0.2 | savg-translate " + hklCoords[i] + " >> molecule.savg")
+	#file2.write(hklRealCoords[i][0] + " " + hklRealCoords[i][1] + " " + hklRealCoords[i][2])
+	file2.write(hklCoords[i] + "\n")
 
 os.system("irisfly --ex molecule.savg")
+os.system("irisfly --ex lines.txt")
+
+file.close()
+file2.close()
