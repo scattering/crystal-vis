fe32a29b1fefd73a671b31356c9737a8c0941d3c
Telon Yan <tjy2@twostep.cam.nist.gov>
2018-06-27 09:18:30 -0400

    deleted old files

--- xyz/build.sh
+++
@@ -1,6 +0,0 @@
-savg-sphere | savg-scale 0.1 | savg-translate 0.50000 0.50000 0.35973
-savg-sphere | savg-scale 0.1 | savg-translate 0.00000 0.00000 0.00000
-savg-sphere | savg-scale 0.1 | savg-translate 0.25000 0.25000 0.00000
-savg-sphere | savg-scale 0.1 | savg-translate 0.00000 0.00000 0.17385
-savg-sphere | savg-scale 0.1 | savg-translate 0.25000 0.25000 0.25000
-savg-sphere | savg-scale 0.1 | savg-translate 0.07347 0.07347 0.17349
